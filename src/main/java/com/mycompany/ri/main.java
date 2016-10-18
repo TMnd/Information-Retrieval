cdfdf648f681ce626067904d6b24e361677063ff
MRodrigues <r.mafalda@ua.pt>
2016-10-18 09:59:48 +0100

    1 hashmap
    1 hashmap

--- src/main/java/com/mycompany/ri/main.java
+++ src/main/java/com/mycompany/ri/main.java
@@ -5,6 +5,13 @@
  */
 package com.mycompany.ri;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 /**
  *
  * @author joaoa
@@ -13,8 +20,27 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        HashMap<ZipEntry, HashMap<String, String>> table = new HashMap<>();
+        ZipFile zipFile = new ZipFile("C:\\Users\\Mafalda Rodrigues\\Desktop\\Mestrado\\RI\\corpus-RI.zip");
+
+        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+        while(entries.hasMoreElements()){
+            ZipEntry entry = entries.nextElement();
+            InputStream stream = zipFile.getInputStream(entry);
+            table.put(entries.nextElement(), new HashMap<>());
+            //System.out.println(entries.nextElement().toString()); 
+        }
+        System.out.println(table);
+        
+        
+        
+        
+    
+               
+                
     }
     
 }
